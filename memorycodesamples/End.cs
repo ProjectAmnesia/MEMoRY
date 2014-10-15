c16d9f7aeeeded29501947bce159df06994d2ba5
HelenaLindell <lindell.helena@hotmail.com>
2014-10-15 14:21:45 +0200

    Change

---
+++ memorycodesamples/End.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryCodeSamples
+{
+    public partial class End : Form
+    {
+
+        internal string winner;
+
+
+        public End()
+        {
+            InitializeComponent();
+
+            lblWinner.Text = winner;
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+
+       
+    }
+}
