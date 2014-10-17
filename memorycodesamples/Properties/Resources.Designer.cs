1568e19120595aeeb11b418aae6e568dd6a7611b
Genrael <nerendor@hotmail.com>
2014-10-17 10:36:45 +0200

    sort of fix

--- memorycodesamples/Properties/Resources.Designer.cs
+++ memorycodesamples/Properties/Resources.Designer.cs
@@ -990,45 +990,6 @@ namespace MemoryCodeSamples.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap orm {
-            get {
-                object obj = ResourceManager.GetObject("orm", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap owl {
-            get {
-                object obj = ResourceManager.GetObject("owl", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
-        /// </summary>
-        internal static System.IO.UnmanagedMemoryStream Pair_correct {
-            get {
-                return ResourceManager.GetStream("Pair_correct", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap panda {
-            get {
-                object obj = ResourceManager.GetObject("panda", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap pandared {
             get {
                 object obj = ResourceManager.GetObject("pandared", resourceCulture);
